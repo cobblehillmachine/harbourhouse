682c04df7919239c706544e145526f21e640fd34
samanthavandapuye <samantha.vandapuye@gmail.com>
2013-11-05 12:43:48 -0500

    supporters update

--- wp-content/themes/harbourhouse/page-our-supporters.php
+++ wp-content/themes/harbourhouse/page-our-supporters.php
@@ -1,12 +1,21 @@
 <?php get_header(); ?>
 	<div class="mid-cont">
 		<div class="page-title"><?php the_title(); ?></div>
-		<?php query_posts(array('post_type' => 'Supporters', 'orderby'=> 'title',  'order' => 'ASC', 'posts_per_page' => 100)); ?>
-		<div id="supporters-cont">
+		<?php query_posts(array('post_type' => 'Supporters', 'orderby'=> 'title',  'order' => 'ASC', 'cat' => 4, 'posts_per_page' => 100)); ?>
+		<div class="supporters-cont">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<a class="supporter" href="<?php the_field('supporter_site'); ?>" target="_blank">
 					<div class="supporter-name"><span><?php the_title(); ?></span></div>
 					<?php if(has_post_thumbnail()) { ?><div class="supporter-logo hidden"><?php the_post_thumbnail('full'); ?></div><?php } ?>
+				</a>
+			<?php endwhile; wp_reset_query(); ?>	
+		</div>
+		
+		<?php query_posts(array('post_type' => 'Supporters', 'orderby'=> 'title',  'order' => 'ASC', 'cat' => 5, 'posts_per_page' => 100)); ?>
+		<div class="supporters-cont">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<a class="supporter-small" href="<?php the_field('supporter_site'); ?>" target="_blank">
+					&nbsp;<strong>&raquo;</strong> <?php the_title(); ?>
 				</a>
 			<?php endwhile; wp_reset_query(); ?>	
 		</div>	
